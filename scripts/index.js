52867a5381450bc49a22f43b396dcd43faf8000c
MigelG <za4elovek@yandex.ru>
2021-08-20 07:21:04 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,34 @@
+//Объявляем переменные
 let popup = document.querySelector('.popup');
 let openPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = document.querySelector('.popup__close-button');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelectorAll('.popup__input')[0];
-let jobInput = document.querySelectorAll('.popup__input')[1];
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
 let profileName = document.querySelector('.profile__username');
 let profilJob = document.querySelector('.profile__job');
 
-function togglePopup() {
-    if (!popup.classList.contains('.popup__opened')) {
-        nameInput.value = profileName.textContent;
-        jobInput.value = profilJob.textContent;
-    }
-    popup.classList.toggle('popup_opened');
+//Функция открытия попапа
+function openPopup() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profilJob.textContent;
+    popup.classList.toggle('popup_opened', true);
 }
 
-function formSubmitHandler(evt) {
+//Функция закрытия попапа
+function closePopup() {
+    popup.classList.toggle('popup_opened', false);
+}
+
+//Функция отправки формы
+function submitForm(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profilJob.textContent = jobInput.value;
-    togglePopup();
+    closePopup();
 }
 
-openPopupButton.addEventListener('click', togglePopup);
-closePopupButton.addEventListener('click', togglePopup);
-formElement.addEventListener('submit', formSubmitHandler);
+//Обработчики событий
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', submitForm);
