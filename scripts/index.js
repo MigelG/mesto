5a675a1dad0e2bdb23d2544b2defe91c7c9fe65d
MigelG <za4elovek@yandex.ru>
2021-09-01 12:40:40 +0300

    feat: реализовать удаление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-//Объявляем переменные
+//Попап
 let popup = document.querySelector('.popup');
 let openPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = document.querySelector('.popup__close-button');
@@ -13,12 +13,12 @@ function openPopup() {
     nameInput.value = profileName.textContent;
     jobInput.value = profilJob.textContent;
     popup.classList.add('popup_opened');
-}
+};
 
 //Функция закрытия попапа
 function closePopup() {
     popup.classList.remove('popup_opened');
-}
+};
 
 //Функция отправки формы
 function submitForm(evt) {
@@ -26,9 +26,56 @@ function submitForm(evt) {
     profileName.textContent = nameInput.value;
     profilJob.textContent = jobInput.value;
     closePopup();
-}
+};
 
 //Обработчики событий
 openPopupButton.addEventListener('click', openPopup);
 closePopupButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', submitForm);
+
+//Карточки мест
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+const deleteButton = document.querySelectorAll('.place__delete-button');
+const places = document.querySelector('.places');
+const placeTemplate = document.querySelector('#place').content;
+
+//Инициализация массива карточек
+initialCards.forEach((card) => {
+    addPlace(card);
+});
+
+//Функция добавления места
+function addPlace(card) {
+    const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
+    placeElement.querySelector('.place__title').textContent = card.name;
+    placeElement.querySelector('.place__image').src = card.link;
+    placeElement.querySelector('.place__delete-button').addEventListener('click', (event) => {
+        event.target.closest('.place').remove();
+    })
+    places.prepend(placeElement);
+};
